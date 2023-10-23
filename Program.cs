1f05e9b74827af823827206b96313fde5b7aa543
Noctis2632 <pitsanukorn.mala@bumail.net>
2023-10-23 22:25:36 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GradingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            string scoreInput = Console.ReadLine();
+            Grading.GradeCalculator(scoreInput);
+        }
+    }
+}
